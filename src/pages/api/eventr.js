ebf99bde2ca7d51f7b42c8c1c661faabf892b079
Basir Khan <khanbasir5555@gmail.com>
2023-12-31 20:03:55 +0530

    redirection issue fixed

--- src/pages/api/eventr.js
+++ src/pages/api/eventr.js
@@ -122,33 +122,7 @@ const handler = async (req, res) => {
       }
     }
     if (req.body.estatus == "old") {
-      const info = await transporter.sendMail({
-        from: "<support@InnovateU.com>", // sender address
-        to: `${req.body.email}`, // list of receivers
-        subject: `🎉 Welcome to InnovateU! Your Account is Ready 🚀`, // Subject line
-        text: "Account Created Successfully", // plain text body
-        html: `
-        <table cellpadding="0" cellspacing="0" width="100%" bgcolor="#f4f4f4">
-<tr>
-    <td style="padding: 20px 0;">
-        <table align="center" cellpadding="0" cellspacing="0" width="600" style="background-color: #ffffff; border-radius: 5px; box-shadow: 0 3px 6px rgba(0, 0, 0, 0.1);">
-            <tr>
-                <td style="padding: 40px 20px; text-align: center;">
-                    <img src="https://res.cloudinary.com/dawzncoau/image/upload/v1695498332/mfourzez3vwheixn36ql.png" alt="InnovateU Logo Logo" width="150">
-                    <h1 style="font-size: 24px; margin-top: 30px; color: #333;">Welcome to InnovateU</h1>
-                    <p style="font-size: 16px; color: #666; margin-top: 20px;">You have successfully created your account on InnovateU.</p>
-                    <p style="font-size: 16px; color: #666;">Start managing your projects more efficiently and collaboratively with our powerful tools.</p>
-                    <a href="" style="display: inline-block; margin-top: 30px; padding: 15px 30px; background-color: #007bff; color: #fff; text-decoration: none; border-radius: 5px; font-size: 16px;">Login to InnovateU</a>
-                    <p style="font-size: 14px; color: #999; margin-top: 20px;">If you have any questions or need assistance, feel free to contact our support team at <a href="mailto:support@projectstudio.com" style="color: #007bff;">support@InnovateU.com</a>.</p>
-                    <p style="font-size: 14px; color: #999;">Thank you for choosing InnovateU!</p>
-                </td>
-            </tr>
-        </table>
-    </td>
-</tr>
-</table>
-        `,
-      });
+      
     }
   } else {
     res
